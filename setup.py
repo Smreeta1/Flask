03d833649b57c28481c30290d59d17258d8e6f05
smreeta <smreetashrestha1@gmail.com>
2024-07-10 00:08:34 +0545

    flask basic site

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask_app',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+    ],
+)

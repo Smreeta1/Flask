73290c269db0a4c3aad254ce4be29a072cb7135c
smreeta <smreeta.191543@ncit.edu.np>
2024-09-07 20:00:57 +0545

    Add Docker Compose setup for hello and Flask services

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simple-project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Flask==3.0.3', 
+    ],
+    
+)

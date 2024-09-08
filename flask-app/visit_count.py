68d92a4b8c87eb8b93f68232132c01c67bab73ed
smreeta <smreeta.191543@ncit.edu.np>
2024-09-08 18:00:20 +0545

    add session and user_id

--- flask-app/visit_count.py
+++ flask-app/visit_count.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask,session
 import redis
+import os
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('SECRET_KEY') 
 cache = redis.Redis(host='redis', port=6379)
+
+@app.route('/')
+def home():
+    return "Welcome to the visit counter!"
 
 @app.route('/visit')
 def hello():
-    count = cache.incr('visits')
-    return f'You visited this page {count} times.'
+    if 'user_id' not in session:
+        session['user_id'] = os.urandom(16).hex()  # Create a unique user ID
+
+    user_id = session['user_id']
+    count = cache.incr(f'visits:{user_id}')  # Increment visit count for this user
+    
+    # count = cache.incr('visits')
+    return f'User ID: {user_id}. You visited this page {count} times.'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
+
